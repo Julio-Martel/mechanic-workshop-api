d39846fdae6a2e103c9050cc54d255eb340f2fc3
Julio-Martel <julioramdom123@gmail.com>
2026-07-25 18:11:39 -0300

    Se ha hecho parte de la estructura principal de la ruta del cliente

---
+++ routes/cliente.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { Router } from 'express';
+
+const clienteRoutes = express.Router();
+
+/*
+    Completar aca todas las rutas del cliente
+
+*/
+
+export default clienteRoutes;
