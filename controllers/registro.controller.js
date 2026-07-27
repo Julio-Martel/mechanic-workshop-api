7e1a01f0c417bcbd5f6fa51354f93fffe9a655de
Julio-Martel <julioramdom123@gmail.com>
2026-07-27 14:20:28 -0300

    Se ha cambiado el error del res que indica que el cliente ya existe

--- controllers/registro.controller.js
+++ controllers/registro.controller.js
@@ -10,9 +10,22 @@ const registroController = async(req,res) => {
 
     } catch(error) {
         if(error.message === 'FORMULARIO VACIO'){
-            return res.status()
+            return res.status(400).json({
+                mensaje: 'No se puede registrar sin los datos.'
+            })
         }
 
+        if(error.message === 'FORMULARIO INCOMPLETO'){
+            return res.status(400).json({
+                mensaje: 'No se pueden mandar formulario con datos incompletos'
+            })
+        }
+
+        if(error.message === 'CLIENTE YA REGISTRADO'){
+            return res.status(400).json({
+                mensaje: 'Cliente ya registrado'
+            })
+        }
 
         res.status(500).json({
             mensaje: 'ERROR INTERNO',
