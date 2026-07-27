dc7106c82d12e952a7bfc590f11d4e9e82c3d060
Julio-Martel <julioramdom123@gmail.com>
2026-07-27 15:55:49 -0300

    Se ha creado la ruta de modificacion de clientes

---
+++ routes/modificar.route.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import express from 'express';
+
+const modificacionRoute = express.Router();
+
+modificacionRoute.patch('/:id', /*AGREGAR HANDLER DE MODIFICACION DE CLIENTE*/);
+
+export default modificacionRoute;
