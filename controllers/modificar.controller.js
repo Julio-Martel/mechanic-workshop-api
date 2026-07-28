2a06b715d92a4f6bc5ae47fdeab385ab574acf8e
Julio-Martel <julioramdom123@gmail.com>
2026-07-28 19:53:39 -0300

    Se ha completado la parte del catch del controlador de modificar datos del usuario

---
+++ controllers/modificar.controller.js
@@ -0,0 +1,26 @@
+import { modificarServices } from "../services/modificar.services.js"
+
+const modificarController = async(req,res) => {
+    try{
+        const {id} = req.params;
+        const clienteModificado = await modificarServices(id);
+
+
+
+
+    } catch(error){
+        if(error.message = 'CLIENTE NO ENCONTRADO'){
+            return res.status(404).json({
+                mensaje: 'El cliente no fue encontrado'
+            })
+        }
+
+        res.status(500).json({
+            mensaje: 'ERROR INTERNO'
+        })
+    }
+}
+
+export {
+    modificarController
+}
