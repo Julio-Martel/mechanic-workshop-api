916fa68f50c4ae41b2fb664b4f31d7c4b349a92d
Julio-Martel <julioramdom123@gmail.com>
2026-07-30 18:12:21 -0300

    se ha borrado una funcionalidad en la exportacion de funciones del modelo

--- models/cliente.model.js
+++ models/cliente.model.js
@@ -34,8 +34,8 @@ const modificarCliente = async(data,id) => {
 }
 
 export {
-    registrarCliente,
     encontrarCliente,
     verificarClientePorDni,
-    registrarCliente
+    registrarCliente,
+    modificarCliente
 }
