53ae9533df51a68685df81cea4454641e3646c77
Julio-Martel <julioramdom123@gmail.com>
2026-07-30 16:24:12 -0300

    Se ha completado el service de modificacion de datos del cliente

--- services/modificar.services.js
+++ services/modificar.services.js
@@ -3,47 +3,16 @@ import { modificarCliente, encontrarCliente } from "../models/modificar.model.js
 
 const modificarServices = async(id,data) => {
     const clienteEncontrado = await encontrarCliente(id);
-    const valores = [];
 
-
-    if(clienteEncontrado.length === undefined){
+    if(clienteEncontrado === undefined){
         throw new Error(`CLIENTE NO ENCONTRADO`);
     }
 
-    if(data.nombre){
-        const nombre = 'nombre = ?';
-        valores.push(nombre);
+    if(!data || Object.keys(data).length === 0){
+        throw new Error(`BODY VACIO`);
     }
 
-    if(data.apellido){
-        const apellido = 'apellido = ?';
-        valores.push(apellido);
-    }
-
-    if(data.dni){
-        const dni = 'dni = ?';
-        valores.push(dni);
-    }
-
-    if(data.telefono){
-        const telefono = 'telefono = ?';
-        valores.push(telefono);
-    }
-
-
-    if(data.email){
-        const email = 'email = ?';
-        valores.push(email);
-    }
-
-    if(valores.length === 0){
-        throw new Error('BODY VACIO');
-    }
-
-
-    const parametros = valores.join();
-
-    const modCliente = await modificarCliente(parametros);
+    const modCliente = await modificarCliente(data,id);
 
     return modCliente;
 
