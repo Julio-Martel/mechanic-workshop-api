b84e74675aba5ced5199c130f98d82b1b8da1cb7
Julio-Martel <julioramdom123@gmail.com>
2026-07-30 18:01:15 -0300

    Se han agregado todas las funcionalidades para el apartado del cliente

---
+++ models/cliente.model.js
@@ -0,0 +1,42 @@
+import db from '../config/db.js';
+
+const registrarCliente = async(data) => {
+    const [resultado] = await db.query(`INSERT INTO Clientes(nombre,apellido,dni,telefono,email)
+        VALUES(?,?,?,?,?)`,
+        [data.nombre,
+         data.apellido,
+         data.dni,
+         data.telefono,
+         data.email]);
+
+    return resultado[0];
+}
+
+
+const verificarClientePorDni = async(dni) => {
+    const [resultado] = await db.query(`SELECT * FROM Clientes 
+        WHERE dni = ?`,[dni]);
+
+    return resultado[0];
+}
+
+const encontrarCliente = async(id) => {
+    const [resultado] = await db.query(`SELECT * FROM Clientes 
+        WHERE id = ?`,[id]);
+
+    return resultado[0];
+}
+
+const modificarCliente = async(data,id) => {
+    const [resultado] = await db.query(`UPDATE Clientes SET ? 
+        WHERE id = ?`,[data,id]);
+
+    return resultado;
+}
+
+export {
+    registrarCliente,
+    encontrarCliente,
+    verificarClientePorDni,
+    registrarCliente
+}
