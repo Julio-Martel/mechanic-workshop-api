915bcfbddeb936eb2eb2dd6df04aea4a7ff42dc2
Julio-Martel <julioramdom123@gmail.com>
2026-07-31 15:53:08 -0300

    Se han agregado los comentarios de referencia para la retomacion del codigo

--- routes/vehiculos.routes.js
+++ routes/vehiculos.routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 const vehiculosRoutes = express.Router();
 
 // RUTA DE REGISTRO DE VEHICULO
-
+vehiculosRoutes.post('/registro', /*AGREGAR HANDLER DE REGISTRO*/);
 
 
 export default vehiculosRoutes;
