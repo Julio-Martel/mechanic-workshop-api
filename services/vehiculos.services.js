170179ef734132844712d86fba8493af684e9091
Julio-Martel <julioramdom123@gmail.com>
2026-08-01 17:54:40 -0300

    Se han agregado dos condicionales mas que faltaban

--- services/vehiculos.services.js
+++ services/vehiculos.services.js
@@ -2,6 +2,14 @@ import { encontrarCliente } from "../models/cliente.model.js"
 import { registroVehiculoModel } from "../models/vehiculos.models.js";
 
 const registroVehiculoService = async(data) => {
+    if(!data || Object.keys(data).length === 0){
+        throw new Error("BODY VACIO");
+    }
+
+    if(!data.id_cliente || !data.marca || !data.anio || !data.patente || !data.color){
+        throw new Error("CAMPOS INCOMPLETOS");
+    }
+
     const clienteRegistrado = await encontrarCliente(data.id_cliente);     
 
     if(!clienteRegistrado){
