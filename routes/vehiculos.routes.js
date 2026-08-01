aafcb3e57cf28445c1f86179ab89cfe0768a4e20
Julio-Martel <julioramdom123@gmail.com>
2026-08-01 18:49:53 -0300

    Se ha completado la ruta de modificacion de vehiculo agregando el handler correspondiente

--- routes/vehiculos.routes.js
+++ routes/vehiculos.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registroController } from '../controllers/vehiculos.controller.js';
+import { registroController, modificacionController } from '../controllers/vehiculos.controller.js';
 
 const vehiculosRoutes = express.Router();
 
@@ -7,5 +7,7 @@ const vehiculosRoutes = express.Router();
 vehiculosRoutes.post('/registro', registroController);
 
 // RUTA DE MODIFICACION DE DATOS DE UN VEHICULO
+vehiculosRoutes.patch('/:id', modificacionController);
+
 
 export default vehiculosRoutes;
