810244a69058e10c8c7553f655405ab46d035109
Julio-Martel <julioramdom123@gmail.com>
2026-08-01 18:13:31 -0300

    Se han añadido comentarios a la ruta

--- routes/vehiculos.routes.js
+++ routes/vehiculos.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import { registroController } from '../controllers/vehiculos.controller.js';
 
 const vehiculosRoutes = express.Router();
 
-// RUTA DE REGISTRO DE VEHICULO
-vehiculosRoutes.post('/registro', /*AGREGAR HANDLER DE REGISTRO*/);
+// RUTA DE REGISTRO DE UN VEHICULO
+vehiculosRoutes.post('/registro', registroController);
 
+// RUTA DE MODIFICACION DE DATOS DE UN VEHICULO
 
 export default vehiculosRoutes;
