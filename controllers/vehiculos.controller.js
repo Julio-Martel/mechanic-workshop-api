d1d8ca3ebfd70cda33c974cf72d4160351fbb4a1
Julio-Martel <julioramdom123@gmail.com>
2026-08-01 17:36:07 -0300

    Se han agregado algunas condicionales al catch del controlador del registro

--- controllers/vehiculos.controller.js
+++ controllers/vehiculos.controller.js
@@ -1,9 +1,24 @@
+import { registroVehiculoService } from "../services/vehiculos.services.js"
 
 const registroController = async(req,res) => {
     try{
+        const vehiculoRegistrado = await registroVehiculoService(req.body);
+
+        res.status(202).json({
+            mensaje: 'VEHICULOS REGISTRADO CON EXITO',
+            datos: vehiculoRegistrado
+        })
 
     } catch(error){
-        
+        if(error.message === 'ID INEXISTENTE'){
+            return res.status(404).json({
+                mensaje: 'El ID del cliente no existe'
+            })
+        }
+
+        res.status(500).json({
+            mensaje: 'ERROR INTERNO'
+        });       
     }
 }
 
