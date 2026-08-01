1f36d0031562b7b6b8113000e152a227e7d478b8
Julio-Martel <julioramdom123@gmail.com>
2026-08-01 17:42:23 -0300

    Se agregado el handler de la ruta

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import clientesRoutes from './routes/clientes.routes.js';
+import vehiculosRoutes from './routes/vehiculos.routes.js';
 
 const app = express();
 
@@ -8,5 +9,7 @@ app.use(express.json());
 //RUTA CRUD DE CLIENTES
 app.use('/clientes', clientesRoutes);
 
+//RUTA CRUD DE VEHICULOS
+app.use('/vehiculos', vehiculosRoutes);
 
 export default app;
