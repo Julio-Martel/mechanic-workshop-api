b4ec31300ddb06f98a6f6adc48bc4e9e919b0047
Julio-Martel <julioramdom123@gmail.com>
2026-08-05 20:55:11 -0300

    Se ha completado el controlador de registro del mecanico

--- controllers/mecanicos.controllers.js
+++ controllers/mecanicos.controllers.js
@@ -2,9 +2,28 @@ import { registroMecanicoService } from "../services/mecanicos.services.js"
 
 const registroMecanicoController = async(req,res) => {
     try {
+        const registrarMecanico = await registroMecanicoService(req.body);
+
+        res.status(202).json({
+            mensaje: 'Mecanico registrado con exito!'
+        })
 
     } catch(error){
+        if(error.message === 'BODY VACIO'){
+            return res.status(403).json({
+                mensaje: 'No se permite mandar el body vacio'
+            })
+        }
 
+        if(error.message === 'YA REGISTRADO'){
+            return res.status(403).json({
+                mensaje: 'El mecanico ya esta registrado'
+            })
+        }
+
+        res.status(505).json({
+            mensaje: 'ERROR INTERNO'
+        });
     }
 }
 
