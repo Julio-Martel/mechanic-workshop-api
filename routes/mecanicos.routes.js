2329eb73d22b8504c9c847701562d3cc887330dc
Julio-Martel <julioramdom123@gmail.com>
2026-08-04 22:09:06 -0300

    Se ha implementado la rutas de la opcion de mecanico

---
+++ routes/mecanicos.routes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const mecanicosRoutes = express.Router();
+
+mecanicosRoutes.post('/registro',/*AGREGAR HANDLER DE REGISTRO DE MECANICOS*/);
+
+export default mecanicosRoutes;
+
