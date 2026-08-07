e24dfd89a1797e6615870f18d7ecdd947251f632
Julio-Martel <julioramdom123@gmail.com>
2026-08-07 19:44:28 -0300

    Se ha creado el archivo de rutas para la creacion de servicios

---
+++ routes/servicios.routes.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const servicioRoutes = express.Router();
+
+//RUTA CREACION DE SERVICIO
+servicioRoutes.post('/',/*AGREGAR HANDLER DE CREACION DE SERVICIO*/);
+
+
+
+export default servicioRoutes;
