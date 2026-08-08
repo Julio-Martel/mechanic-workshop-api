6faedb2cc6e6d512044c96edb9ac1eb9990360ad
Julio-Martel <julioramdom123@gmail.com>
2026-08-08 17:46:42 -0300

    Se ha creado la estructura principal del controlador de la creacion de servicios

--- controllers/servicios.controllers.js
+++ controllers/servicios.controllers.js
@@ -1,5 +1,12 @@
 const crearServicioController = async(req,res) => {
-
+    try {
+        
+    } catch(error){
+        console.log(error);
+        res.status(505).json({
+            mensaje: 'ERROR INTERNO'
+        })
+    }
 }
 
 export {
