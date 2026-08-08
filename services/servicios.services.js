5da8305784d834305848465d138773a48ccc451d
Julio-Martel <julioramdom123@gmail.com>
2026-08-08 18:43:44 -0300

    Se han importado las funcionalidades del model de creacion de servicios

--- services/servicios.services.js
+++ services/servicios.services.js
@@ -1,7 +1,19 @@
-const crearServicioService = async() => {
+import { servicioDuplicadoPorDescripcion, 
+         crearServicioModel } from "../models/servicios.models";
+
+const crearServicioService = async(data) => {
+    if(!data || Object.keys(data).length === 0){
+        throw new Error("BODY VACIO");
+    }
+
+    if(!data.nombre || !data.descripcion || !data.precio){
+        throw new Error("DATOS INCOMPLETOS");
+    }
+
+
 
 }
 
 export {
-    crearServicioService;
+    crearServicioService
 }
