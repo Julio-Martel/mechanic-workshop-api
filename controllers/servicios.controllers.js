31f4c98c614d9ae032d6c32ea48bde522947f784
Julio-Martel <julioramdom123@gmail.com>
2026-08-08 21:02:36 -0300

    Se han agregado las condicionales faltantes para el catch

--- controllers/servicios.controllers.js
+++ controllers/servicios.controllers.js
@@ -1,8 +1,26 @@
+import { crearServicioService } from "../services/servicios.services.js";
+
 const crearServicioController = async(req,res) => {
     try {
-        
+        const servicioCreado = await crearServicioService(req.body);
+    
+        res.status(202).json({
+            mensaje: 'Servicio creado con exito!'
+        })
+
     } catch(error){
-        console.log(error);
+        if(error.message === 'BODY VACIO'){
+            return res.status(403).json({
+                mensaje: 'No se puede mandar el body vacio'
+            })
+        }
+
+        if(error.message === 'SERVICIO DUPLICADO'){
+            return res.status(403).json({
+                mensaje: 'El servicio ya se encuentra'
+            })
+        }
+
         res.status(505).json({
             mensaje: 'ERROR INTERNO'
         })
