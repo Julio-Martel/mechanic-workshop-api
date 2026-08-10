f88e327529cea0e1060ec3fbc9ef8a04d09c95c0
Julio-Martel <julioramdom123@gmail.com>
2026-08-10 19:02:33 -0300

    Se ha completado el controlador de modificacion del servicio

--- controllers/servicios.controllers.js
+++ controllers/servicios.controllers.js
@@ -1,4 +1,5 @@
-import { crearServicioService } from "../services/servicios.services.js";
+import { crearServicioService,
+        modificacionSerivicioService } from "../services/servicios.services.js";
 
 const crearServicioController = async(req,res) => {
     try {
@@ -21,6 +22,48 @@ const crearServicioController = async(req,res) => {
             })
         }
 
+        console.log(error);
+
+        res.status(505).json({
+            mensaje: 'ERROR INTERNO'
+        })
+    }
+}
+
+const modificacionSerivicioController = async(req,res) => {
+    try{
+        const {id} = req.params;
+        const modificarServicio = await modificacionSerivicioService(id,req.body);
+        
+        res.status(202).json({
+            mensaje: `Servicio con ID: ${id}, actualizado correctamente`
+        })
+
+    } catch(error){
+        if(error.message === 'FALTA ID'){
+            return res.status(403).json({
+                mensaje: 'Debe ingresar el ID en la url para poder actualizar el servicio.'
+            })
+        }
+        
+        if(error.message === 'BODY VACIO'){
+            return res.status(403).json({
+                mensaje: 'No puede modificar el servicio si no manda datos.'
+            })
+        }
+    
+        if(error.message === 'DATOS INCOMPLETOS'){
+            return res.status(403).json({
+                mensaje: 'Debe mandar al menos un dato para poder modificar el servicio.'
+            })
+        }
+
+        if(error.message === 'INEXISTENTE'){
+            return res.status(404).json({
+                mensaje: 'ID de servicio no encontrado'
+            })
+        }
+
         res.status(505).json({
             mensaje: 'ERROR INTERNO'
         })
@@ -28,5 +71,6 @@ const crearServicioController = async(req,res) => {
 }
 
 export {
-    crearServicioController
+    crearServicioController,
+    modificacionSerivicioController
 }
