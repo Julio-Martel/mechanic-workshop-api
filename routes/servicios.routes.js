0dafea5cc59d801d2678f5bd018bae72a19c6c5b
Julio-Martel <julioramdom123@gmail.com>
2026-08-10 19:07:10 -0300

    Se ha completado la ruta de modificacion de servicios

--- routes/servicios.routes.js
+++ routes/servicios.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import { crearServicioController, modificacionSerivicioController } from '../controllers/servicios.controllers.js';
 
 const servicioRoutes = express.Router();
 
 //RUTA CREACION DE SERVICIO
-servicioRoutes.post('/',/*AGREGAR HANDLER DE CREACION DE SERVICIO*/);
+servicioRoutes.post('/',crearServicioController);
+
+//RUTA EDICION DE SERVICIOS
+servicioRoutes.post('/:id',modificacionSerivicioController);
 
 
 
