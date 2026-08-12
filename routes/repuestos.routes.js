431d9e0fe3fc28aa510a9f5636d71a3b6f11f2b5
Julio-Martel <julioramdom123@gmail.com>
2026-08-12 16:36:41 -0300

    Se creo el archivo de rutas de repuestos y se ha completado parcialmente el archivo

---
+++ routes/repuestos.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const repuestosRoutes = express.Router();
+
+//RUTA REGISTRO DE REPUESTO
+repuestosRoutes.post('/', /*AGREGAR HANDLER REGISTRO DE REPUESTO*/);
+
+
+
+
+
+export default repuestosRoutes;
