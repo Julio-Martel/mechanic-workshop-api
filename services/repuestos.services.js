e113cbac27639bf6ce076117fabab104b78d9fa4
Julio-Martel <julioramdom123@gmail.com>
2026-08-13 19:02:02 -0300

    Se ha completado el service de registro de repuesto

--- services/repuestos.services.js
+++ services/repuestos.services.js
@@ -1,7 +1,17 @@
-import { registrarRepuestoModel } from "../models/repuestos.models.js";
+import { registrarRepuestoModel,
+        encontrarRepuestoPorNombre
+ } from "../models/repuestos.models.js";
 
-const registrarRepuestoService = async() => {
-    
+const registrarRepuestoService = async(data) => {
+    const repuestoDuplicadoPorNombre = await encontrarRepuestoPorNombre(data.nombre);
+
+    if(repuestoDuplicadoPorNombre){
+        throw new Error("Nombre duplicado. Ingrese otro.");
+    }
+
+    const registrarRepuesto = await registrarRepuestoModel(data);
+
+    return registrarRepuesto;
 }
 
 export {
