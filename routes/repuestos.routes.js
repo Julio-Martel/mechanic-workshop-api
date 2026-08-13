6668acecc3e3aee28736ea8cf0cad099956c8212
Julio-Martel <julioramdom123@gmail.com>
2026-08-13 19:50:48 -0300

    Se han agregado las demas rutas pero faltan agregar sus respectivos handlers

--- routes/repuestos.routes.js
+++ routes/repuestos.routes.js
@@ -9,7 +9,10 @@ repuestosRoutes.post('/', registrarRepuestoController);
 //RUTA ACTUALIZAR STOCK DE REPUESTO
 repuestosRoutes.patch('/:id',/*AGREGAR HANDLER DE ACUTUALIZACION DE STOCK*/);
 
+//RUTA CONSULTAR DISPONIBILIDAD DE REPUESTO
+repuestosRoutes.get();
 
-
+//RUTA DE ELIMINACION DE UN REPUESTOS
+repuestosRoutes.delete('/:id',/*AGREGAR HANDLER DE ELIMINACION DE REPUESTOS*/);
 
 export default repuestosRoutes;
