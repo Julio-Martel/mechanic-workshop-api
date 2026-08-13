c6114add663e61f943815850acb3be4678720153
Julio-Martel <julioramdom123@gmail.com>
2026-08-12 21:44:40 -0300

    Se ha complementado el registro model de repuestos

--- models/repuestos.models.js
+++ models/repuestos.models.js
@@ -1,7 +1,14 @@
 import db from '../config/db.js';
 
-const registrarRepuestoModel = async() => {
+const registrarRepuestoModel = async(data) => {
+    const [resultados] = await db.query(`INSERT INTO Repuestos(nombre,stock,precio_unitario)
+        VALUES(?,?,?)`,[
+            data.nombre,
+            data.stock,
+            data.precio_unitario
+        ]);
 
+    return resultados.insertId;
 }
 
 
