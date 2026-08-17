41c267da9adce5f63c3fc6a4eee0e1347cef6149
Julio-Martel <julioramdom123@gmail.com>
2026-08-17 20:57:24 -0300

    Se han agregado todas las rutas del crud de orden, pero faltan agregarles sus respectivos handlers

--- routes/ordenes.routes.js
+++ routes/ordenes.routes.js
@@ -2,7 +2,22 @@ import express from 'express';
 
 const ordenesRoutes = express.Router();
 
+//RUTA CREAR ORDEN
+ordenesRoutes.post('/crear', /*AGREGAR HANDLER DE CREACION DE ORDEN*/);
 
+//RUTA ASIGNACION VEHICULO
+ordenesRoutes.post('/asignarVehiculo', /*AGREGAR HANDLER DE ASIGNACION DE VEHICULO*/);
 
+//RUTA ASIGNACION MECANICO
+ordenesRoutes.post('/asignarMecanico', /*AGREGAR HANDLER DE ASIGNACION DE */)
+
+//RUTA CAMBIO DE ESTADO DE ORDEN
+ordenesRoutes.path('/cambiar', /*AGREGAR HANDLER DE CAMBIO DE ESTADO*/);
+
+//RUTA CONSULTAR ORDEN
+ordenesRoutes.get('/:id', /*AGREGAR HANDLER DE CONSULTA DE ORDEN*/);
+
+//RUTA CANCELAR ORDEN
+ordenesRoutes.patch('/:id', /*AGREGAR HANDLER DE CANCELACION DE ORDEN*/);
 
 export default ordenesRoutes;
