fabbbd7d0bc565f75f1ac79ffd00d95f9c4c4bfd
Julio-Martel <julioramdom123@gmail.com>
2026-08-19 19:04:35 -0300

    Se ha implementado y terminado la estructura completa del controlador de creacion de orden

---
+++ controllers/ordenes.controllers.js
@@ -0,0 +1,21 @@
+import { crearOrdenServices } from "../services/ordenes.services.js";
+
+const crearOrdenController = async(req,res) => {
+    try {
+        const ordenCreada =  await crearOrdenServices(req.body);
+        
+        res.status(202).json({
+            mensaje: 'Orden creada con exito.'
+        })
+    
+    } catch(error){
+        res.status(505).json({
+            mensaje: 'ERROR INTERNO'
+        })
+    }
+}
+
+
+export {
+    crearOrdenController
+}
