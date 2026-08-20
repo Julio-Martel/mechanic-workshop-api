27300ce14925b08f7d9d2b68fb14460ac2b3e3c7
Julio-Martel <julioramdom123@gmail.com>
2026-08-20 19:18:14 -0300

    Se han completado los dos controlador de crear y cambiar el estado

--- controllers/ordenes.controllers.js
+++ controllers/ordenes.controllers.js
@@ -1,4 +1,6 @@
-import { crearOrdenServices } from "../services/ordenes.services.js";
+import { crearOrdenServices,
+         cambiarEstadoService
+} from "../services/ordenes.services.js";
 
 const crearOrdenController = async(req,res) => {
     try {
@@ -10,6 +12,71 @@ const crearOrdenController = async(req,res) => {
         })
     
     } catch(error){
+        if(error.message === 'BODY VACIO'){
+            return res.status(403).json({
+                mensaje: 'No se pueden mandar el body vacio'
+            })
+        }
+    
+        if(error.message === 'DEBE MANDAR TODOS LOS DATOS'){
+            return res.status(403).json({
+                mensaje: 'Debe mandar todos los datos'
+            })
+        }
+
+        if(error.message === 'ID VEHICULO NO EXISTE"'){
+            return res.status(404).json({
+                mensaje: 'Vehiculo no encontrado con ese id'
+            })
+        }
+
+        if(error.message === 'ID MECANICO NO EXISTE'){
+            return res.status(404).json({
+                mensaje: 'Mecanico no encontrado con ese id'
+            })
+        }
+
+        if(error.message === 'SIN CAMBIOS'){
+            return res.status(403).json({
+                mensaje: 'No se han podido realizar los cambios'
+            })
+        }
+        res.status(505).json({
+            mensaje: 'ERROR INTERNO'
+        })
+    }
+}
+
+const cambiarEstadoController = async(req,res) => {
+    try {
+        const {id} = req.params;
+
+        const estadoCambiado = await cambiarEstadoService(id,req.body);
+
+        res.status(202).json({
+            mensaje: 'Estado de orden actualizado.',
+            estadoCambiado: estadoCambiado
+        })
+
+    } catch(error){
+        if(error.message === 'BODY VACIO'){
+            return res.status(403).json({
+                mensaje: 'Se debe mandar el estado'
+            });
+        }
+
+        if(error.message === 'ESTADO INCORRECTO'){
+            return res.status(403).json({
+                mensaje: 'Debe mandar un estado valido: pendiente, en reparacion, finalizada o cancelada'
+            })
+        }
+
+        if(error.message === 'NO SE PUEDE CAMBIAR ESTADO'){
+            return res.status(403).json({
+                mensaje: 'No se puede modificar el estado de una orden finaliza o cancelada'
+            })
+        }
+
         res.status(505).json({
             mensaje: 'ERROR INTERNO'
         })
@@ -18,5 +85,6 @@ const crearOrdenController = async(req,res) => {
 
 
 export {
-    crearOrdenController
+    crearOrdenController,
+    cambiarEstadoController
 }
