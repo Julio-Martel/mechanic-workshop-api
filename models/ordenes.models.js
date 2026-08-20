d26639ff3f53512c288d80ff59551bc0793c692d
Julio-Martel <julioramdom123@gmail.com>
2026-08-19 22:10:04 -0300

    Se ha creado la funcionalidad de crear orden

--- models/ordenes.models.js
+++ models/ordenes.models.js
@@ -1,9 +1,10 @@
 import db from '../config/db.js';
 
-const crearOrdenModel = async() => {
-    const [resultado] = await db.query()
+const crearOrdenModel = async(data) => {
+    const [resultado] = await db.query(`INSERT INTO Orden(id_vehiculo, id_mecanico, fecha_ingreso, fecha_entrega, estado)
+        VALUES(?,?,?,?,?)`,[data.id_mecanico, data.id_vehiculo, data.fecha_ingreso, data.fecha_entrega, data.estado]);
 
-
+    return resultado.affectedRows;
 }
 
 export {
