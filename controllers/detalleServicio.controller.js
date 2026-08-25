40d4433afbfcd4f54ee4d2653940660534f9b137
Julio-Martel <julioramdom123@gmail.com>
2026-08-25 19:11:52 -0300

    Se ha completado el controlador de creacion de detalle del servicio

--- controllers/detalleServicio.controller.js
+++ controllers/detalleServicio.controller.js
@@ -9,6 +9,30 @@ const crearDetalleServicioController = async(req,res) => {
         })
     
     }catch(error){
+        if(error.message === 'BODY VACIO'){
+            return res.status(403).json({
+                mensaje: 'No se puede mandar el body vacio'
+            })
+        }
+
+        if(error.message === 'DATOS INCOMPLETOS'){
+            return res.status(403).json({
+                mensaje: 'Se deben mandar todos los datos'
+            })
+        }
+
+        if(error.message === 'ID INEXISTENTE ORDEN'){
+            return res.status(404).json({
+                mensaje: 'El id de orden no existe.'
+            })
+        }
+
+        if(error.message === 'ID INEXISTENTE SERVICIO'){
+            return res.status(404).json({
+                mensaje: 'El id de servicio no existe.'
+            })
+        }
+
         res.status(505).json({
             mensaje: 'ERROR INTERNO'
         })
