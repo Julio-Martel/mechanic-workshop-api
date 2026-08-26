fc3d1ba26e5bf8cdacb21541c3019039e0c913c5
Julio-Martel <julioramdom123@gmail.com>
2026-08-26 15:22:54 -0300

    Se ha completado el model de crear detalle del repuesto

--- models/detalleRepuesto.models.js
+++ models/detalleRepuesto.models.js
@@ -1,5 +1,14 @@
-const crearDetalleRepuestoModel = async(data) => {
+import db from '../config/db.js';
 
+const crearDetalleRepuestoModel = async(data) => {
+    const [resultado] = await db.query(`INSERT INTO DetalleRepuesto(id_orden, id_repuesto, cantidad)
+        VALUES(?,?,?)`,[
+            data.id_orden,
+            data.id_repuesto,
+            data.cantidad
+        ]);
+
+    return resultado.affectedRows;
 }
 
 export {
