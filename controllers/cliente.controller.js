eba413c6fa5738ef38cdb7393e2aab875473fe3a
Julio-Martel <julioramdom123@gmail.com>
2026-08-31 14:33:19 -0300

    Se cambiado el controlador de registro del cliente

--- controllers/cliente.controller.js
+++ controllers/cliente.controller.js
@@ -5,13 +5,14 @@ import { registroService, modificarServices,
 const registroController = async(req,res) => {
     try {
         const clienteRegistrado = await registroService(req.body);    
-        
+
         res.status(201).json({
             mensaje: 'Cliente registrado con exito!'
         })
 
     } catch(error) {
-        if(error.message === 'FORMULARIO VACIO'){
+        console.log(error)
+        if(error.message === 'BODY VACIO'){
             return res.status(400).json({
                 mensaje: 'No se puede registrar sin los datos.'
             })
@@ -19,19 +20,18 @@ const registroController = async(req,res) => {
 
         if(error.message === 'FORMULARIO INCOMPLETO'){
             return res.status(400).json({
-                mensaje: 'No se pueden mandar formulario con datos incompletos'
+                mensaje: 'Faltan datos obligatorios'
             })
         }
 
         if(error.message === 'CLIENTE YA REGISTRADO'){
-            return res.status(400).json({
-                mensaje: 'Cliente ya registrado'
+            return res.status(409).json({
+                mensaje: 'DNI o Email ya existentes'
             })
         }
 
         res.status(500).json({
-            mensaje: 'ERROR INTERNO',
-            error: error
+            mensaje: 'ERROR INTERNO'
         })
     }    
 }
