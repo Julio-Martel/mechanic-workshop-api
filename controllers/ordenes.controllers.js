1bd842942a6fa043458d8e98f0b612328d7c5b75
Julio-Martel <julioramdom123@gmail.com>
2026-09-19 21:38:29 -0300

    Se ha eliminado un comentario

--- controllers/ordenes.controllers.js
+++ controllers/ordenes.controllers.js
@@ -65,11 +65,6 @@ const crearOrdenController = async(req,res) => {
     }
 }
 
-
-/*
-    ORDEN DE REPARACION ACTIVA ESTA NO APARECE EN EL LISTADO
-*/
-
 const cambiarEstadoController = async(req,res) => {
     try {
         const {id} = req.params;
