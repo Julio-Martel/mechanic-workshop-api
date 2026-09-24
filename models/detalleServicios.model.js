3fddf022d9c78f24c32159d7ae90ed6a0a78d62a
Julio-Martel <julioramdom123@gmail.com>
2026-09-24 20:56:07 -0300

    Se ha cambiado el nombre de la funcion

--- models/detalleServicios.model.js
+++ models/detalleServicios.model.js
@@ -11,6 +11,12 @@ const crearDetalleServicio = async(data) => {
     return resultado.affectedRows;
 }
 
+const todosLosDetallesServiciosModel = async() => {
+    const [resultados] = await db.query(`SELECT * FROM Detalles`);
+
+    return resultados[0];
+}
+
 const serviciosAsociadosAUnaOrden = async(id_orden) => {
     const [resultados] = await db.query(`SELECT COUNT(*) AS total FROM Detalle
         WHERE id_orden = ?`,
